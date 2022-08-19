72c408b0af09bd1452406abd368aa2b1d42e3dd0
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-08-18 20:28:57 -0500

    build: init project with `create-react-app`

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='container text-center'>
+      <span className='text-primary'>
+        <i className='bi bi-wifi-2 fs-1' />
+      </span>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
+    </main>
+  );
+}
+
+export default App;
